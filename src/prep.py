1bd43f4dc3e3d14f2d5402a63fdec8e6507db862
Muhsigbokz <muhsigbokz@googlemail.com>
2022-06-01 17:08:27 +0200

    wip

--- src/prep.py
+++ src/prep.py
@@ -39,8 +39,8 @@ def macro_from_path(js_file_name, pack, src):
     macro = load(open(f"{src}/{pack}/default.json"))
     js_file_content = open(js_file_path).read()
     macro["command"] = js_file_content
-    macro["_id"] = id_from_js_file_content(js_file_content)
-    macro["name"] = name_from_js_file_content(js_file_content)
+    macro["_id"] = id_from_js_file_content(js_file_content, js_file_name)
+    macro["name"] = name_from_js_file_content(js_file_content, js_file_name)
     return macro
 
 
